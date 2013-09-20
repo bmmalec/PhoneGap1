772a0801e807535b51075cad1213d524b7095e45
bmmalec <bmmalec@msn.com>
2013-09-20 07:11:45 -0500

    dev 1

---
+++ scripts/GapJS.js
@@ -0,0 +1,7 @@
+// JavaScript source code
+var GapJS = (function(module) {
+    module.Event1 = function(resultObject) {
+        resultObject.prepend(Date() + '<br/>');
+    };
+    return module;
+} (GapJS || {}));
